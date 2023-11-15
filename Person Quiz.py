cb106ea7dcce5b99ba939730e8440532c0cff8be
dnsgud <dnsgud0302@gmail.com>
2023-11-15 15:23:07 +0900

    사진 확장자 수정

--- Person Quiz.py
+++ Person Quiz.py
@@ -4,7 +4,7 @@ from PIL import Image
 directory_path = '"C:\Users\Desktop\인물 퀴즈\인물 사진"'  # 디렉토리 경로를 지정함.
 def load_random_image(directory_path) :  # 지정된 경로에서 모든 파일 목록을 불러옴
     file_list = os.listdir(directory_path)
-    image_files = [file for file in file_list if file.lower().endswith('.jpg')]  # 확장자가 ‘jpg’인 이미지파일만 불러옴
+    image_files = [file for file in file_list if file.lower().endswith('.jpeg')]  # 확장자가 ‘jpeg’인 이미지파일만 불러옴
 
 random_image_file = random.choice(image_files)  # 무작위로 이미지를 선택
 
