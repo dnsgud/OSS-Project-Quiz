5e5411232413782e9776e4e3ab45c4a1e27eb2ff
MoccaLatteng <147587831+Cross1406@users.noreply.github.com>
2023-11-15 15:01:03 +0900

    Update Proverb Quiz.py

--- Proverb Quiz.py
+++ Proverb Quiz.py
@@ -1,7 +1,14 @@
-dict_eng = {"가는 날이" : "장날이다", "싼 게" : "비지떡", "백지장도" : "맞들면 낫다", "계란으로" : "바위치기"}
-for i in dict_eng:
-    user_input = input(f"{i}")
-    if user_input == dict_eng[i]:
+import random
+
+proverb_kor = {"가는 날이": "장날이다", "싼 게": "비지떡", "백지장도": "맞들면 낫다", "계란으로": "바위치기"}
+
+proverbs = list(proverb_kor.items())
+random.shuffle(proverbs)
+
+for i, (x, y) in enumerate(proverbs, start=1):
+    user_input = input(f"{i}. {x} ")
+    if user_input.strip() == y:  
         print("정답입니다")
     else:
-        print(f"틀렸습니다. 정답은 {dict_eng[i]}입니다.")
+        print(f"틀렸습니다. 정답은 {y}입니다.")
+
