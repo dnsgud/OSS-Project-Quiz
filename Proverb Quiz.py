e2961086217a89dba39784217be8711c7aa2d0fb
MoccaLatteng <147587831+Cross1406@users.noreply.github.com>
2023-11-20 10:02:00 +0900

    Update Proverb Quiz.py

--- Proverb Quiz.py
+++ Proverb Quiz.py
@@ -1,14 +1,42 @@
+import linecache
 import random
 
-proverb_kor = {"가는 날이": "장날이다", "싼 게": "비지떡", "백지장도": "맞들면 낫다", "계란으로": "바위치기"}
+def generate_quiz():
+    # 'saying.txt' 파일에서 무작위로 한 줄을 읽어옵니다
+    no = random.randint(1, 100)
+    saying = linecache.getline('saying.txt', no).strip()
+    return saying
 
-proverbs = list(proverb_kor.items())
-random.shuffle(proverbs)
+def create_quiz(saying):
+    # 속담에서 뒷부분을 네모 느낌의 빈칸으로 대체합니다
+    words = saying.split()
+    last_word = words[-1]
+    words[-1] = '□' * len(last_word)  # 네모 느낌의 빈칸으로 대체합니다
+    return " ".join(words), last_word
 
-for i, (x, y) in enumerate(proverbs, start=1):
-    user_input = input(f"{i}. {x} ")
-    if user_input.strip() == y:  
-        print("정답입니다")
-    else:
-        print(f"틀렸습니다. 정답은 {y}입니다.")
+def main():
+    score = 0
 
+    while True:
+        # 속담 퀴즈를 생성하고 사용자에게 제시합니다
+        proverb = generate_quiz()
+        quiz, answer = create_quiz(proverb)
+
+        # 사용자에게 직접 입력을 받을 때 빈칸의 개수에 맞게 입력받도록 합니다
+        while True:
+            user_input = input(f"문제 {score + 1}: 다음 속담을 완성하세요: '{quiz}' ").strip()
+            if len(user_input) == len(answer):
+                break
+            else:
+                print(f"입력한 글자의 개수가 맞지 않습니다. 다시 입력하세요.")
+
+        # 정답을 체크하고 피드백을 제공합니다 (공백은 무시하여 비교)
+        if user_input.replace(" ", "") == answer.replace(" ", ""):
+            print("정답입니다!")
+            score += 1
+        else:
+            print(f"틀렸습니다. 정답은 '{answer}'입니다.")
+            break  # 틀리면 프로그램 종료
+
+if __name__ == "__main__":
+    main()
