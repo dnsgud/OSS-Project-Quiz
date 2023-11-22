cac8e5da56c95fc30cd5284bb138be9a4cb467c2
dnsgud <dnsgud0302@gmail.com>
2023-11-22 12:37:42 +0900

    복수정답도 정답으로 처리하는 기능

--- Person Quiz.py
+++ Person Quiz.py
@@ -4,21 +4,21 @@ from PIL import Image
 import threading
 import time
 
-directory_path = r"C:\Users\jung1\Desktop\인물 퀴즈\인물 사진"  # 디렉토리 경로를 지정함
-time_limit = 5  # 문제의 제한 시간
+directory_path = r"C:\Users\jung1\Desktop\인물 퀴즈\인물 사진"  # 디렉토리 경로를 지정
+time_limit = 5  # 각 문제의 제한 시간
 
 def get_user_input(timeout, default=None):
     result = [default]
     
     def user_input():
-        result[0] = input("인물의 이름을 입력 : ")
+        result[0] = input("인물의 이름을 입력 : ")  # 답변 입력
 
     thread = threading.Thread(target=user_input)
     thread.start()
     thread.join(timeout)
 
-    if thread.is_alive():
-        print("\n시간이 초과되었습니다.")
+    if thread.is_alive():   #시간 초과시 오답 처리
+        print("\n시간이 초과되었습니다. 정답은 {}입니다.".format(random_image_file.replace(".jpeg", "").replace(",", " ")))
         return None
     else:
         return result[0]
@@ -27,7 +27,7 @@ def load_random_image(directory_path):
     # 지정된 경로에서 모든 파일 목록을 불러옴
     file_list = os.listdir(directory_path)
 
-    # 확장자가 ‘.jpeg’인 이미지 파일만 불러옴
+    # 확장자가 ‘.jpeg’인 이미지 파일을 불러옴
     image_files = [file for file in file_list if file.lower().endswith('.jpeg')]
 
     if not image_files:
@@ -40,7 +40,7 @@ def load_random_image(directory_path):
         # 무작위로 이미지를 선택
         random_image_file = random.choice(image_files)
 
-        # 선택된 이미지의 전체 경로를 작성함
+        # 선택된 이미지의 전체 경로 작성
         image_path = os.path.join(directory_path, random_image_file)
 
         # Pillow를 사용하여 이미지를 열음
@@ -51,17 +51,17 @@ def load_random_image(directory_path):
         person_name = get_user_input(time_limit)
 
         if person_name is None:
-            break  # 시간 초과시 퀴즈 종료
+            break  # 시간 초과로 게임 종료
 
-        # 파일명과 입력 받은 이름이 일치하는지 확인
-        if person_name.lower() in random_image_file.lower():
+        # 파일명과 입력 받은 이름이 정답들 중 하나와 일치하는지 확인
+        if any(person_name.lower() == answer.lower() for answer in random_image_file.split(',')):
             print("정답입니다.")
             score += 1  # 정답일 경우 점수 1점 증가
         else:
-            print("오답입니다. 정답은 {}입니다.".format(random_image_file.replace(".jpeg", "")))
+            print("오답입니다. 정답은 {}입니다.".format(random_image_file.replace(".jpeg", "").replace(",", " ")))
             break  # 틀릴 경우 반복 종료
 
-    print("점수: {}".format(score))
+    print("점수: {}".format(score)) #최종 점수
 
 # 함수 호출
 load_random_image(directory_path)
