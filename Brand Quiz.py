d788a84f085f2054e0c8134b291de16d628c9d56
남현빈 <84217238+0301qls@users.noreply.github.com>
2023-12-05 09:22:44 +0900

    Update Brand Quiz.py

--- Brand Quiz.py
+++ Brand Quiz.py
@@ -1,51 +1,43 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-import os
-import random
-
-class BrandLogoQuiz:
-    def __init__(self, root, logo_directory):
-        # (이전 코드 생략)
-
-        # 로고 이미지 열기
-        self.logo_path = os.path.join(self.logo_directory, self.current_logo_file)
-        self.logo_image = Image.open(self.logo_path)
-
-        # 로고 이미지에서 자표에 해당하는 부분을 잘라냄
-        self.coordinates = (100, 50, 300, 250)
-        self.cropped_image = self.logo_image.crop(self.coordinates)
-
-        # Tkinter PhotoImage 객체로 변환
-        self.tk_cropped_image = ImageTk.PhotoImage(self.cropped_image)
-
-        # 원본 로고 이미지 Tkinter PhotoImage 객체로 변환
-        self.tk_original_image = ImageTk.PhotoImage(self.logo_image.resize(self.cropped_image.size))
-
-        # (이후 코드 생략)
+         self.next_question()
 
     def next_question(self):
-        # 새로운 로고 퀴즈로 이동
         self.select_next_logo()
+
         self.logo_path = os.path.join(self.logo_directory, self.current_logo_file)
         self.logo_image = Image.open(self.logo_path)
-
-        # 로고 이미지에서 자표에 해당하는 부분을 잘라냄
         self.cropped_image = self.logo_image.crop(self.coordinates)
+        self.q_pixmap = self.pil_to_pixmap(self.cropped_image)
 
-        # Tkinter PhotoImage 객체로 변환
-        self.tk_cropped_image = ImageTk.PhotoImage(self.cropped_image)
+        self.logo_label.setPixmap(self.q_pixmap)
 
-        self.logo_label.config(image=self.tk_cropped_image)  # 로고 이미지 업데이트
+        self.entry.clear()
+        self.reset_countdown()
+        self.entry.setDisabled(False)  # 입력 창 활성화
+        self.submit_button.setDisabled(False)  # 제출 버튼 활성화
+        self.countdown_timer.start()
 
-        self.entry.delete(0, tk.END)  # 텍스트 입력 상자 초기화
+    def update_countdown(self):
+        self.countdown -= 1
+        self.countdown_label.setText(f"남은 시간: {self.countdown}초")
 
-        # 7초 카운트 다운 시작
-        self.root.after(0, self.countdown, 7)
+        if self.countdown == 0:
+            self.countdown_timer.stop()
+            self.entry.setDisabled(True)  # 입력 창 비활성화
+            self.submit_button.setDisabled(True)  # 제출 버튼 비활성화
+            self.show_game_over()
 
-# (이후 코드 생략)
+    def reset_countdown(self):
+        self.countdown = 5
+        self.countdown_label.setText(f"남은 시간: {self.countdown}초")
+        self.countdown_timer.start()
 
-# 특정 디렉토리에서 로고 이미지 파일로 퀴즈 생성
-logo_directory = "image"  # 실제 디렉토리 경로로 대체
-root = tk.Tk()
-quiz_app = BrandLogoQuiz(root, logo_directory)
-root.mainloop()
+    def show_game_over(self):
+        game_over_message = QMessageBox(self.root)
+        game_over_message.setWindowTitle("게임 종료")
+        game_over_message.setText(f"게임 종료! 최종 점수: {self.score}")
+        game_over_message.exec_()
+        self.app.quit()
+
+if __name__ == "__main__":
+    logo_directory = "image"  # 실제 디렉토리 경로로 대체
+    quiz_app = BrandLogoQuiz(logo_directory)
