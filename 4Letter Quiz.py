d131682930929e23fad80eb5a04957d6c437df47
junsu park <109206192+greentea5713@users.noreply.github.com>
2023-12-10 22:26:04 +0900

    메뉴버튼 추가

--- 4Letter Quiz.py
+++ 4Letter Quiz.py
@@ -6,10 +6,16 @@ import random
 class FourletterQuizGame(QWidget):
     def __init__(self):
         super().__init__()
+
         self.quiz_data, self.answer_data = self.load_quiz_data()
         self.quiz_data, self.answer_data = self.shuffle_quiz_data(self.quiz_data, self.answer_data)
+
         self.total_score = 0
         self.current_index = 0
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.handle_timeout)
+
         self.init_ui()
 
     def init_ui(self):
@@ -23,10 +29,13 @@ class FourletterQuizGame(QWidget):
         self.layout.addWidget(self.score_label, alignment=Qt.AlignmentFlag.AlignCenter)
         self.quiz_label = QLabel(self)
         self.layout.addWidget(self.quiz_label, alignment=Qt.AlignmentFlag.AlignCenter)
-
         self.answer_input = QLineEdit(self)
         self.answer_input.returnPressed.connect(self.check_answer)
         self.layout.addWidget(self.answer_input, alignment=Qt.AlignmentFlag.AlignCenter)
+        # 메인 화면으로 돌아가는 버튼 추가
+        self.retry_button = QPushButton("다시하기", self)
+        self.retry_button.clicked.connect(self.show_main_menu)
+        self.layout.addWidget(self.retry_button, alignment=Qt.AlignmentFlag.AlignCenter)
         self.show_question()
         self.show()
     def load_quiz_data(self):
